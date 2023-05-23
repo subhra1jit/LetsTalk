77384db9b51449e08e99780a6874e5fd097c0f92
subhrajitb123 <subhrajitb@letsupgrade.in>
2023-05-23 11:01:21 +0530

    heroicon module added and template changes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+
+const App=()=>
+{
+    return(
+        <Router>
+            <Routes>
+                <Route/>
+            </Routes>
+        </Router>
+    )
+}
+
+export default App;
